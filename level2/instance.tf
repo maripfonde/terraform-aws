1509fd0ed549f310e255dcc083c5ec69806e4a1a
lama <lawr.styles@gmail.com>
2022-12-13 19:00:48 +0200

    Add AWS SSM Session Manager

--- level2/instance.tf
+++ level2/instance.tf
@@ -1,61 +1,7 @@
-resource "aws_instance" "public" {
-  ami                         = "ami-0b0dcb5067f052a63"
-  associate_public_ip_address = true
-  instance_type               = "t3.micro"
-  key_name                    = "main"
-  vpc_security_group_ids      = [aws_security_group.public.id]
-  subnet_id                   = data.terraform_remote_state.level1.outputs.public_subnet_id[1]
-
-  tags = {
-    Name = "${var.env_code}-public"
-  }
-}
-
-resource "aws_security_group" "public" {
-  name        = "${var.env_code}-public"
-  description = "Allow inbound traffic"
-  vpc_id      = data.terraform_remote_state.level1.outputs.vpc_id
-
-  ingress {
-    description = "HTTP from public"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "SSH from public"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["197.221.253.109/32"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "${var.env_code}-pubic"
-  }
-}
-
 resource "aws_security_group" "private" {
   name        = "${var.env_code}-private"
   description = "Allow VPC traffic"
   vpc_id      = data.terraform_remote_state.level1.outputs.vpc_id
-
-  ingress {
-    description = "SSH from VPC"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [data.terraform_remote_state.level1.outputs.vpc_cidr]
-  }
 
   ingress {
     description     = "HTTP from load balancer"
